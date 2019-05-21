0d61d803ab2e98db5da7adf5bcd5bee0a19f6fe9
adann0 <40341834+adann0@users.noreply.github.com>
2019-05-21 20:05:23 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,4 +4,5 @@ VERSION=Squeak5.2-18229-32bit-201810190412-ARMv6
 
 wget http://files.squeak.org/5.2/Squeak5.2-18229-32bit/$VERSION.zip &&
 unzip $VERSION.zip &&
-$VERSION/bin/squeak -vm-sound-null -vm-display-null -headless $VERSION/shared/Squeak5.2-18229-32bit.image install.sh
+$VERSION/bin/squeak $VERSION/shared/Squeak5.2-18229-32bit.image install.sh
+#$VERSION/bin/squeak -vm-sound-null -vm-display-null -headless $VERSION/shared/Squeak5.2-18229-32bit.image install.sh
