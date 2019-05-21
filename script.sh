a782cbbdcad9d74e5e9eef219add5ef44f16f543
adann0 <40341834+adann0@users.noreply.github.com>
2019-05-21 20:49:20 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,5 +8,7 @@ IMAGE=$RELEASE-All-in-One
 
 wget http://files.squeak.org/$VERSION/$RELEASE/$IMAGE.zip &&
 unzip $IMAGE.zip &&
-./$IMAGE.app/Contents/$ARM/bin/squeak -vm-sound-null -vm-display-null -headless $IMAGE.app/Contents/Resources/$RELEASE.image /install.st
+./$IMAGE.app/Contents/$ARM/bin/squeak $IMAGE.app/Contents/Resources/$RELEASE.image /install.st
+#./$IMAGE.app/Contents/$ARM/bin/squeak -vm-sound-null -vm-display-null -headless $IMAGE.app/Contents/Resources/$RELEASE.image /install.st
+
 #$VERSION/bin/squeak -vm-sound-null -vm-display-null -headless $VERSION/shared/Squeak5.2-18229-32bit.image install.sh
