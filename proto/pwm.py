c01f8f9688210d5dfe003748c39ce22ec75c5d54
adann0 <40341834+adann0@users.noreply.github.com>
2019-07-27 12:43:31 +0200

    Update pwm.py

--- proto/pwm.py
+++ proto/pwm.py
@@ -7,13 +7,13 @@ import pigpio
 def usage(msg) : sys.exit(msg)
 
 def rotate(pin, sleep) :
-  pi = pigpio.pi() # Connect to local Pi.
-  pi.set_mode(pin, pigpio.OUTPUT)
-  pi.set_servo_pulsewidth(pin, 1500)
-  time.sleep(sleep)
-  pi.set_servo_pulsewidth(pin, 0)
-  pi.stop()
+    pi = pigpio.pi() # Connect to local Pi.
+    pi.set_mode(pin, pigpio.OUTPUT)
+    pi.set_servo_pulsewidth(pin, 1500)
+    time.sleep(sleep)
+    pi.set_servo_pulsewidth(pin, 0)
+    pi.stop()
 
 if __name__ == "__main__" :
-   if len(sys.argv) < 3 : usage("USAGE: ./pwm <pin> <seconds>")
-   rotate(int(sys.argv[1]), int(sys.argv[2]))
+    if len(sys.argv) < 3 : usage("USAGE: ./pwm <pin> <seconds>")
+    rotate(int(sys.argv[1]), int(sys.argv[2]))
